5804e8c55558b36a35b399fe79f3fe8be9228d1e
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-09-26 13:32:53 -0300

    First Commit

---
+++ Projeto_Banking/Projeto_Banking.Testes/ExemploTesting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto_Banking.Models;
+
+namespace Projeto_Banking.Testes
+{
+    [TestClass]
+    public class ExemploTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            Assert.AreEqual(Exemplo.soma(2, 3), 5);
+        }
+    }
+}
