83cc359d4d510cea33e04ae6a634079e39fcf0b0
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-09-27 15:12:28 -0300

    Carregando Conta

---
+++ Projeto_Banking/Projeto_Banking/Models/ContaDAOs/ContaDAO.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_Banking.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto_Banking.Models.ContaDAOs
+{
+    public class ContaDAO
+    {
+        public Conta PesquisarContaPorNumero(int numero)
+        {
+            float saldo = 0;
+            MySqlCommand command = Connection.Instance.CreateCommand();
+            command.CommandText = "SELECT * FROM projetobanking.conta WHERE Conta_id = @id;";
+            command.Parameters.AddWithValue("@id", numero);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                saldo = float.Parse(reader["Conta_saldo"].ToString());
+            }
+            reader.Close();
+
+            ContaCorrente cc = new ContaCorrenteDAO().PesquisarPorNumero(numero);
+            if (cc != null)
+            {
+                cc.Saldo = saldo;
+                return cc;
+            }
+            return null;
+        }
+    }
+}
