87e7fe3d140889034082ef0d7c2db98c693b79ea
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-09-27 13:04:36 -0300

    Atualizando Banco

---
+++ Projeto_Banking/Projeto_Banking/Models/Conta/ContaCorrenteDAO.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_Banking.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto_Banking.Models.Conta
+{
+    public class ContaCorrenteDAO
+    {
+        public ContaCorrente Login(ContaCorrente cc)
+        {
+            MySqlCommand command = Connection.Instance.CreateCommand();
+            command.CommandText = "SELECT * FROM projetobanking.conta_corrente WHERE Conta_Conta_Corrente ;";
+            return null;
+
+        }
+    }
+}
