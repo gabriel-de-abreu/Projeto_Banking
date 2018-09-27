2db6fc43c80e25f6c2965a5b2306047bb3f9388b
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-09-27 16:29:20 -0300

    Iniciando Pagamento DAO

---
+++ Projeto_Banking/Projeto_Banking/Models/Opecacoes/Emprestimo/PagamentoDAOs/PagamentoDAO.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_Banking.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto_Banking.Models.Opecacoes.Emprestimo
+{
+    public class PagamentoDAO
+    {
+        public Pagamento Inserir(Pagamento pagamento)
+        {
+            MySqlCommand command = Connection.Instance.CreateCommand();
+            command.CommandText = "INSERT INTO `ProjetoBanking`.`Pagamento` (`Pagamento_id`, `Pagamento_data`, `Emprestimo_Emprestimo_id`, `Pagamento_Valor`)" +
+                " VALUES (@data, @emprestimo_id,@valor);";
+            command.Parameters.AddWithValue("@data", pagamento.Data);
+            command.Parameters.AddWithValue("@emprestimo_id", pagamento.Id);
+            command.Parameters.AddWithValue("@valor", pagamento.Valor);
+            command.ExecuteNonQuery();
+            pagamento.Id = Convert.ToInt32(command.LastInsertedId);
+            return pagamento;
+        }
+    }
+}
--- Projeto_Banking/Projeto_Banking/Objetos/PagamentoBoleto.cs
+++ Projeto_Banking/Projeto_Banking/Objetos/PagamentoBoleto.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace Projeto_Banking.Objetos
 {
-    public class PagamentoBoleto
+    public class PagamentoBoleto : Pagamento
     {
         public int Codigo { get; set; }
         public DateTime Vencimento { get; set; }
