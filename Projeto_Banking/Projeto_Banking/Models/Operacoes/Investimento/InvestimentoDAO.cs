536c6b48e6668884a1682cd951fc3127a46561ac
Flávio Vinícius Martins Dos Santos <vinicius0809@gmail.com>
2018-09-28 15:11:08 -0300

    Att

--- Projeto_Banking/Projeto_Banking/Models/Operacoes/Investimento/InvestimentoDAO.cs
+++ Projeto_Banking/Projeto_Banking/Models/Operacoes/Investimento/InvestimentoDAO.cs
@@ -10,39 +10,29 @@ namespace Projeto_Banking.Models.Operacoes.Investimento
 {
     public class InvestimentoDAO
     {
-        public InvestimentoConta InserirInvestimento(Projeto_Banking.Objetos.Investimento investimento, ContaCorrente conta, double valor)
+        public InvestimentoConta InserirInvestimento(InvestimentoConta investimentoConta)
         {
             try
             {
-                MySqlCommand command = Connection.Instance.CreateCommand();
-                string sql = ("INSERT INTO Investimento_Conta (Investimento_Investimento_id, Conta_Corrente_Conta_Conta_Corrente_id," +
-                    " Investimento_Conta_Valor)  VALUES (@Investimento_Investimento_id, @Conta_Corrente_Conta_Conta_Corrente_id," +
-                    " @Investimento_Conta_valor)");
-
-                command.CommandText = sql;
-                command.Parameters.AddWithValue("@Investimento_Investimento_id", investimento.Id);
-                command.Parameters.AddWithValue("@Conta_Corrente_Conta_Conta_Corrente_id", conta.Numero);
-                command.Parameters.AddWithValue("@Investimento_Conta_valor", valor);
-
-                if (conta.Saldo > valor) // Necessário ter saldo suficiente para investir
+                if (investimentoConta.Conta.Saldo > investimentoConta.Valor) // Necessário ter saldo suficiente para investir
                 {
-                    //Antes de criar o investimento no banco de dados, é necessário verificar na conta contábil se a operação
-                    //é valida.
 
-                    // if (ContaContabilInvestimento.VerificaInvestimento(valor)) { 
+                    MySqlCommand command = Connection.Instance.CreateCommand();
+                    string sql = ("INSERT INTO Investimento_Conta (Investimento_Investimento_id, Conta_Corrente_Conta_Conta_Corrente_id," +
+                        " Investimento_Conta_Valor, Investimento_Inicio, Investimento_Fim)  VALUES (@Investimento_Investimento_id, " +
+                        "@Conta_Corrente_Conta_Conta_Corrente_id, @Investimento_Conta_valor, @Investimento_Inicio, @Investimento_Fim)");
+
+                    command.CommandText = sql;
+                    command.Parameters.AddWithValue("@Investimento_Investimento_id", investimentoConta.Investimento.Id);
+                    command.Parameters.AddWithValue("@Conta_Corrente_Conta_Conta_Corrente_id", investimentoConta.Conta.Numero);
+                    command.Parameters.AddWithValue("@Investimento_Conta_valor", investimentoConta.Valor);
+                    command.Parameters.AddWithValue("@Investimento_Inicio", investimentoConta.DataInicio);
+                    command.Parameters.AddWithValue("@Investimento_Fim", investimentoConta.DataFim);
 
                     int retorno = command.ExecuteNonQuery();
 
                     if (retorno > 0)
                     {
-                        InvestimentoConta investimentoConta = new InvestimentoConta()
-                        {
-                            Conta = conta,
-                            Investimento = investimento,
-                            Valor = valor
-                        };
-                        //Caso esteja tudo certo com a operação de inserção de investimento, o mesmo é sensibilizado na Conta Contábil de investimentos
-                        // ContaContabilInvestimento.SensibilizaInvestimento(investimentoConta);
                         return investimentoConta;
                     }
                 }
@@ -77,8 +67,6 @@ namespace Projeto_Banking.Models.Operacoes.Investimento
                     //  investimento.PreFixada = reader["Investimento_nome"].ToString();
                     investimento.Rentabilidade = double.Parse(reader["Investimento_rentabilidade"].ToString());
                     investimento.Taxa = new TaxaDAO().PesquisarPorTaxa(int.Parse(reader["Taxa_Taxa_id"].ToString()));
-                    investimento.DataInicio = DateTime.Parse(reader["Investimento_Inicio"].ToString());
-                    investimento.DataFim = DateTime.Parse(reader["Investimento_Fim"].ToString());
                 }
                 reader.Close();
                 return investimento;
--- Projeto_Banking/Projeto_Banking/Objetos/OperacoesObjetos/InvestimentoConta.cs
+++ Projeto_Banking/Projeto_Banking/Objetos/OperacoesObjetos/InvestimentoConta.cs
@@ -10,5 +10,7 @@ namespace Projeto_Banking.Objetos
         public ContaCorrente Conta { get; set; }
         public double Valor { get; set; }
         public Investimento Investimento { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
     }
 }
