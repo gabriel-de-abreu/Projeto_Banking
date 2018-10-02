17a6a493cf78f43224e549c99d86f31974d58ddb
João Victor Ferreira <30957280+joaojvf@users.noreply.github.com>
2018-10-02 16:39:39 -0300

    Criacao do extrato

---
+++ Projeto_Banking/Projeto_Banking/Views/vwsExtrato.aspx.cs
@@ -0,0 +1,39 @@
+﻿using Projeto_Banking.Models;
+using Projeto_Banking.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projeto_Banking.Views
+{
+    public partial class vwsExtrato : System.Web.UI.Page
+    {
+        ContaCorrente cc;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["contaCorrente"] != null)
+            {
+                cc = Session["contaCorrente"] as ContaCorrente;
+                PopularGrid();
+            }
+            else
+            {
+                Response.Redirect("~/Views/vwsLogin.aspx");
+            }
+        }
+
+        public void PopularGrid()
+        {
+            MovimentacaoDAO movDao = new MovimentacaoDAO();
+            DataTable dTable = movDao.BuscarExtratosConta(cc);
+
+            gdvExtrato.DataSource = dTable;
+            gdvExtrato.DataBind();
+        }
+
+    }
+}
